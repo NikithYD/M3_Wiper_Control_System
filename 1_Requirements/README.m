46ce77b12d26ebe9ee755d4d63f53e38856d787d
NikithYD <68335075+NikithYD@users.noreply.github.com>
2022-05-11 13:34:41 +0530

    Create README.m
    yes

---
+++ 1_Requirements/README.m
@@ -0,0 +1,26 @@
+# Introduction
+Wiper system is an important component in all the vehicle system. They consist of ac motor fitted with arm which cleans the windshield surface of both front and rear windshield. Modern day cars also posses automatic sensing operation using Rain sensor which basically consists of LED that detects the amount of water on the windshield.
+
+# Timeline and Implementation
+The Gannt chart below represents the design and implementaion of the project simulation-
+![Wiper System](https://user-images.githubusercontent.com/68335075/167797225-83a0a305-2c61-410f-9775-4be35345715d.png)
+
+# Features
+ * Wiper system which is handy and easy to use
+ * STM32F407 Controller ARM Cortex -M4 which is faster and contains superior and innovative peripherals
+ * Pushbutton to turn on ignition as well as to change the speed
+ * Three LEDs indicating the wiping operation
+# SWOT Analysis
+The chart below shows the SWOT analysis of the project designed-
+![Clean windshield during heavy rains Considered for safety while driving for clear vision](https://user-images.githubusercontent.com/68335075/167798701-fda76dec-218c-42be-8d67-e514635b58eb.png)
+
+# 4Ws and 1Hs
+ * Who- Useful for drivers especially acrs, buses and trucks for clear vision of the road
+ * What- Aim is to implement automatic wiper system for vehicles
+ * When- Very helpful when front winshield is dusty or during rain
+ * Where- Installed at the front windshield operated by driver or through rain sensors
+ * How- Ignition is turned on, the wiper starts its operation along with speed adjustment modes which improves overall operation 
+ 
+# High and Low Level Requirements
+ ## High Level Requirements
+ 
